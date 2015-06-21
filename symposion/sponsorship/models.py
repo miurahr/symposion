464f08f7ffe7f7fb7b8f5610c39efe6f693e3ab0
Hiroshi Miura <miurahr@linux.com>
2015-06-21 14:47:52 +0900

    Sponsorship improvement
    check if completed and introduce option benefit
    Feedback from PyConJP development
    ```
    commit 28864a33583c11f6502d1f80e233599ea36b0848
    Author: MURAOKA Yusuke <yusuke@jbking.org>
    Date:   Mon Mar 31 14:01:52 2014 +0900
    
        introduce an invisible Benefit type, option to manage individual
        sponsor benefit not related to sponsor level
    
    ```
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- symposion/sponsorship/models.py
+++ symposion/sponsorship/models.py
@@ -179,6 +179,19 @@ class Sponsor(models.Model):
     def send_coordinator_emails(self):
         pass  # @@@ should this just be done centrally?
 
+    def benefit_is_complete(self, name):
+        """Return True - benefit is complete, False - benefit is not complete,
+         or None - benefit not applicable for this sponsor's level """
+        if BenefitLevel.objects.filter(level=self.level, benefit__name=name).exists():
+            try:
+                benefit = self.sponsor_benefits.get(benefit__name=name)
+            except SponsorBenefit.DoesNotExist:
+                return False
+            else:
+                return benefit.is_complete
+        else:
+            return None   # Not an applicable benefit for this sponsor's level
+
 
 def _store_initial_level(sender, instance, **kwargs):
     if instance:
@@ -242,11 +255,23 @@ class SponsorBenefit(models.Model):
     text = models.TextField(_("text"), blank=True)
     upload = models.FileField(_("file"), blank=True, upload_to="sponsor_files")
 
+    # Whether any assets required from the sponsor have been provided
+    # (e.g. a logo file for a Web logo benefit).
+    is_complete = models.NullBooleanField(help_text=_(u"True - benefit complete; False - benefit incomplete; Null - n/a"))
+
     class Meta:
         ordering = ["-active"]
 
     def __unicode__(self):
-        return u"%s - %s" % (self.sponsor, self.benefit)
+        return u"%s - %s (%s)" % (self.sponsor, self.benefit,
+                                  self.benefit.type)
+
+    def save(self, *args, **kwargs):
+        # Validate - save() doesn't clean your model by default, so call
+        # it explicitly before saving
+        self.full_clean()
+        self.is_complete = self._is_complete()
+        super(SponsorBenefit, self).save(*args, **kwargs)
 
     def clean(self):
         num_words = len(self.text.split())
@@ -262,6 +287,20 @@ class SponsorBenefit(models.Model):
         """
         if self.benefit.type == "file" or self.benefit.type == "weblogo":
             return ["upload"]
-        elif self.benefit.type == "text":
+        elif self.benefit.type in ("text", "richtext", "simple", "option"):
             return ["text"]
         return []
+
+    def _is_complete(self):
+        return self.active and \
+            ((self.benefit.type in ('text', 'richtext', 'simple') and bool(self.text))
+                or (self.benefit.type in ('file', 'weblogo') and bool(self.upload)))
+
+
+def _denorm_weblogo(sender, instance, created, **kwargs):
+    if instance:
+        if instance.benefit.type == "weblogo" and instance.upload:
+            sponsor = instance.sponsor
+            sponsor.sponsor_logo = instance
+            sponsor.save()
+post_save.connect(_denorm_weblogo, sender=SponsorBenefit)
