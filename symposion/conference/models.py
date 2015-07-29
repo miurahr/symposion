64b9bc41b1b7ab46c40ce9994f8c49620403a7cf
Hiroshi Miura <miurahr@linux.com>
2015-07-29 17:49:53 +0900

    replace to django-timezone-field from -timezones
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- symposion/conference/models.py
+++ symposion/conference/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-from timezones.fields import TimeZoneField
+from timezone_field import TimeZoneField
 
 
 CONFERENCE_CACHE = {}
